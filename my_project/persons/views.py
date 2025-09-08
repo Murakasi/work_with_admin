bef599dd078d849fe84efaf96c4e40fb738dab8d
Ruslan Bihun <bigun.ruslan@gmail.com>
2025-09-08 08:45:10 +0200

    rename static and templates dir in BASE_DIR dir, and create base data for views urls and templates persons app

--- my_project/persons/views.py
+++ my_project/persons/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
+
+def index(request:HttpRequest):
+    context = {
+        
+    }
+    return render(request, "persons/index.html", context)
